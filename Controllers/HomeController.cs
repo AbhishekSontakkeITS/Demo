64796e1ef659e07ab04e38824fc45262b952b6b3
AbhishekSontakkeITS <abhishek.sontakke@intellisourcetech.net>
2023-03-09 10:55:23 -0500

    About Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Demo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About page.";
 
             return View();
         }
